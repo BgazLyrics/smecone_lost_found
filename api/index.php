7cafc2336d6d4fe02301863e7cab5c4cad6a4626
BgazLyrics <bagasseviardanarionaldi@gmail.com>
2026-08-14 18:18:12 +0700

    Fix vercel output directory to public

--- api/index.php
+++ api/index.php
@@ -1,3 +1,27 @@
 <?php
 
+// 1. Arahkan semua folder penyimpanan cache Laravel ke direktori /tmp
+$storageDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/app/public',
+    '/tmp/storage/logs',
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// 2. Set environment variable storage agar Laravel membacanya di /tmp
+putenv('APP_CONFIG_CACHE=/tmp/config.php');
+putenv('APP_EVENTS_CACHE=/tmp/events.php');
+putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
+putenv('APP_ROUTES_CACHE=/tmp/routes.php');
+putenv('APP_SERVICES_CACHE=/tmp/services.php');
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+
+// 3. Muat aplikasi Laravel
 require __DIR__ . '/../public/index.php';
