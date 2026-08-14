b85c4f815592ec0f2bdb83d71b0c95a5853f3379
Bagas Seviardana Rionaldi <139362025+BgazLyrics@users.noreply.github.com>
2026-08-14 20:06:51 +0700

    aa

--- api/index.php
+++ api/index.php
@@ -2,7 +2,9 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Pastikan storage path diarahkan ke /tmp untuk serverless
 $app = require_once __DIR__ . '/../bootstrap/app.php';
+$app->useStoragePath('/tmp/storage');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
